7b780b4536bd4028d5f15ee53cf67342081c5286
Fernando Martinez <mfernandomartinez1@yahoo.com>
2020-03-13 23:59:32 +0100

    Almacenamos la informacion de los vehiculos

---
+++ informacionVehiulos.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+#Fernando Adalid Martinez Diaz
+# -programa : genisys Inc
+# Empresa de servicio de estacionamiento
+#clases a agregar : 
+#numero de placa , marca, Modelo , tipo de vehi
+#hora de ingreso, Estado
+
+from menuCompañiaGenisys 
+import 
